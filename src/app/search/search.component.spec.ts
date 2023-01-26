b8c523cace4340e711c419df0f03d3f3f6448f74
LuanBraga <luanbraga.lb@gmail.com>
2023-01-26 10:43:21 -0300

    execute unit tests and end-to-end tests

--- src/app/search/search.component.spec.ts
+++ src/app/search/search.component.spec.ts
@@ -1,19 +1,40 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
+import { SearchService } from '../shared';
+import { MockActivatedRoute } from '../shared/search/mocks/routes';
 
 import { SearchComponent } from './search.component';
 
 describe('SearchComponent', () => {
   let component: SearchComponent;
   let fixture: ComponentFixture<SearchComponent>;
+  let mockSearchService: SearchService;
+  let mockActivatedRoute: MockActivatedRoute;
 
   beforeEach(async () => {
+    mockActivatedRoute = new MockActivatedRoute({term: 'nikola'});
+
     await TestBed.configureTestingModule({
-      declarations: [ SearchComponent ]
+      declarations: [ SearchComponent ],
+      providers: [
+        {provide: ActivatedRoute, useValue: mockActivatedRoute}
+      ],
+
+      imports: [FormsModule, RouterTestingModule, HttpClientTestingModule]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
+    // mock response
+    mockSearchService = TestBed.inject(SearchService);
+    mockSearchService.search = jasmine.createSpy().and.returnValue(of([]));
+
+    // initialize component
     fixture = TestBed.createComponent(SearchComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -21,5 +42,19 @@ describe('SearchComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should search when a term is set and search() is called', () => {
+    component = fixture.componentInstance;
+    component.query = ']';
+    component.search();
+
+    expect(mockSearchService.search).toHaveBeenCalledWith(']');
+  });
+
+  it('should search automatically when a term is on the URL', () => {
+    fixture.detectChanges();
+
+    expect(mockSearchService.search).toHaveBeenCalledWith('nikola');
   });
 });
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { Person, SearchService } from '../shared';
 
 @Component({
@@ -6,13 +8,29 @@ import { Person, SearchService } from '../shared';
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent implements OnInit, OnDestroy {
+  sub!: Subscription;
   query!: string;
   searchResults: Person[] = [];
 
-  constructor(private searchService: SearchService) { }
+  constructor(
+    private searchService: SearchService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.sub = this.route.params.subscribe(params => {
+      if(params['term']) {
+        this.query = decodeURIComponent(params['term']);
+        this.search();
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.sub) {
+      this.sub.unsubscribe();
+    }
   }
 
   search(): void { 
